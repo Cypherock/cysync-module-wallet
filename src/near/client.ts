ec50a0bb950170e7a3a1f90fc15eafd6be7936c5
Tejasv Sharma <tejasvonly@gmail.com>
2022-07-20 18:04:19 +0530

    CSI-1027 prettify

--- src/near/client.ts
+++ src/near/client.ts
@@ -1,50 +1,39 @@
 import { AxiosResponse } from 'axios';
 import { near } from '@cypherock/server-wrapper';
 
-export const getBalance = async (
-  address: string,
-  network = 'testnet',
-) => {
+export const getBalance = async (address: string, network = 'testnet') => {
   return near.wallet
     .getBalance({
       address,
-      network,
+      network
     })
     .request()
     .then((res: AxiosResponse) => res.data);
 };
 
-export const getKeys = async (
-  address: string,
-  network = 'testnet',
-) => {
+export const getKeys = async (address: string, network = 'testnet') => {
   return near.wallet
     .getKeys({
       address,
-      network,
+      network
     })
     .request()
     .then((res: AxiosResponse) => res.data);
 };
 
-export const getAccounts = async (
-  address: string,
-  network = 'testnet',
-) => {
+export const getAccounts = async (address: string, network = 'testnet') => {
   return near.wallet
     .getAccounts({
       address,
-      network,
+      network
     })
     .request()
     .then((res: AxiosResponse) => res.data);
 };
 
-export const getBlockHash = async (
-  network = 'testnet',
-) => {
+export const getBlockHash = async (network = 'testnet') => {
   return near.transaction
-    .getBlockHash({network})
+    .getBlockHash({ network })
     .request()
     .then((res: AxiosResponse) => res.data);
 };

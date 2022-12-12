36e88cfd71d62fce25b2ee25c6a89fff2ef4b0d8
Ujjwal Kumar <ujjwal@cypherock.com>
2022-12-12 20:23:34 +0530

    chore: update address derivation to support harmony address

--- src/utils/generateEthAddress.ts
+++ src/utils/generateEthAddress.ts
@@ -1,7 +1,14 @@
 import { utils } from 'ethers';
+import { formatHarmonyAddress } from './formatEthAddress';
 
-const generateEthAddress = (xpub: string) => {
+const generateEthAddress = (
+  xpub: string,
+  coinType: string = 'eth',
+  options?: { forceHex: boolean }
+) => {
   const address = utils.HDNode.fromExtendedKey(xpub).derivePath(`0/0`).address;
+  if (options?.forceHex) return address;
+  if (coinType === 'one') return formatHarmonyAddress(address);
   return address;
 };
 
